d1a6cbe1b01fb279e7ede87935dab81008cf8fb2
G ARUTO <awdalig@gmail.com>
2024-03-13 00:39:55 -0300

    fixed depencies, and running it on shell for full use of virtual environment, mainly focusing on translate.pyfor main application function

--- GoogleTrans/translate.py
+++ GoogleTrans/translate.py
@@ -1,12 +1,11 @@
-
+import googletrans
 from googletrans import *
-from googletrans import Translator
+
 
     
 translator = googletrans.Translator()
 
-trans = translator.translate('Hello there, how are you', dest='korean')
+translate = translator.translate('Hello there, how are you', dest='korean')
 print(trans)
-
 
 
